e14d8e5a6f7c6cf6ad981ba25675b68a0abc3370
Dicluu <alexsezz@list.ru>
2023-10-12 05:45:48 +0300

    add a versioning

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -19,6 +19,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     public const HOME = '/home';
 
+    protected $namespace = 'App\\Http\\Controllers\\';
+
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
      */
@@ -30,10 +32,12 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
+                ->prefix('api/v1')
+                ->namespace($this->namespace . 'Api\\v1')
+                ->group(base_path('routes/api_v1.php'));
 
             Route::middleware('web')
+                ->namespace($this->namespace . 'Api\\v1')
                 ->group(base_path('routes/web.php'));
         });
     }
